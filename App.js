cd944f2f4e012eda3c228cba887a67706057eee7
DILEEP R S <deepurs666@gmail.com>
2023-03-07 11:10:29 +0530

    Update App.js

--- App.js
+++ App.js
@@ -688,28 +688,33 @@ const ResturantList = [
   },
 ];
 
-const RestrauntCard = () => {
+const RestrauntCard = ({
+  name,
+  avgRating,
+  costForTwoString,
+  cloudinaryImageId,
+}) => {
   return (
     <div className="card">
       <img
         src={
           "https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/" +
-          ResturantList[3].data.data.cloudinaryImageId
+          cloudinaryImageId
         }
       />
 
-      <h1>{ResturantList[4].data.data.name}</h1>
+      <h2>{name}</h2>
+      <h2>{avgRating}</h2>
+      <h3>{costForTwoString}</h3>
     </div>
   );
 };
 
 const Body = () => (
   <div className="cardbody">
-    <RestrauntCard />
-    <RestrauntCard />
-    <RestrauntCard />
-    <RestrauntCard />
-    <RestrauntCard />
+    {ResturantList.map((Resturant) => {
+      return <RestrauntCard {...Resturant.data.data} />;
+    })}
   </div>
 );
 
